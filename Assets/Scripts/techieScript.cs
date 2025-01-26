9f4f9c4263b2e465f95fd0432d91e76f43d618f5
Ari's Laptop <151627853+ayshe811@users.noreply.github.com>
2025-01-26 22:44:10 +0000

    Added Particle System to Gems

--- Assets/Scripts/techieScript.cs
+++ Assets/Scripts/techieScript.cs
@@ -8,10 +8,14 @@ public class techieScript : MonoBehaviour
     Color highlight;
 
     AudioSource src;
+    ParticleSystem parSystem;
+    SpriteRenderer sr;
     void Start()
     {
         cursor = GameObject.Find("cursor");
         src = GetComponent<AudioSource>();
+        parSystem = GetComponent<ParticleSystem>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
 
@@ -22,21 +26,28 @@ public class techieScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player") src.PlayOneShot(src.clip);
+        if (collision.gameObject.tag == "Player")
+        {
+            src.PlayOneShot(src.clip);
+            parSystem.Play();
+            sr.enabled = false;
+            Destroy(gameObject, 0.5f);
+
+        }
+
+        //private void OnTriggerEnter2D(Collider2D collision)
+        //{
+        //    if (collision.gameObject.tag == "mouse")
+        //    {
+        //        print("hit!");
+        //        transform.localScale = new Vector3(0.2f, 0.2f);
+        //       // highlight = new Color()
+        //    }
+
+        //    if (collision.gameObject.tag == "Player") src.PlayOneShot(src.clip);
+        //}
+        //private void OnTriggerExit2D(Collider2D collision)
+        //{
+        //    if (collision.gameObject.tag == "mouse") transform.localScale = new Vector3(0.13f, 0.13f);
     }
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "mouse")
-    //    {
-    //        print("hit!");
-    //        transform.localScale = new Vector3(0.2f, 0.2f);
-    //       // highlight = new Color()
-    //    }
-
-    //    if (collision.gameObject.tag == "Player") src.PlayOneShot(src.clip);
-    //}
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "mouse") transform.localScale = new Vector3(0.13f, 0.13f);
 }
