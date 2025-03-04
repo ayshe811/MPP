0b2f78767f854207b3dba0cf388dcbd0e6517dec
ayshe811 <ari.meroa@gmail.com>
2025-03-04 16:43:59 +0000

    bye!

--- Assets/Scripts/collisionManager.cs
+++ Assets/Scripts/collisionManager.cs
@@ -28,7 +28,6 @@ public class collisionManager : MonoBehaviour
         {
             StartCoroutine(shuffle());
             queueScript.UpdateQueueDisplay();
-         //   queueScript.OnShuffleCompleted();
             currentIndex = 0;
             playScript.correctCollision++;
         }
